db8952c9fb59c6f4172e78c6ef6c54b56fd8f00b
Areebey <areebrafiq125@gmail.com>
2023-03-20 04:02:59 +0500

    seclast commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState,useEffect } from 'react';
+import {Link} from "react-router-dom";
 // import cover from "../../Covers/cover1.jpg"
 import "./Home.scss";
 import axios from 'axios';
@@ -6,6 +7,12 @@ const apikey="7502ce33b1fc4368f04531e0e6ab5b5d";
 const url ="https://api.themoviedb.org/3/movie";
 const imgurl="https://image.tmdb.org/t/p/original"
 const upcoming ="upcoming";
+const nowplaying="nowplaying";
+const popular="popular";
+const topRated="top_rated";
+
+
+
 
 const Card = ({img}) =>{
 
@@ -25,7 +32,7 @@ const Row = ({tittle, arr=[{
       {
         arr.map((item,index)=>(
 
-          <Card key={index} img={imgurl+"/"+item.img}/>
+        <Card key={index} img={imgurl+"/"+item.img}/>
         ))
       }
     </div>
@@ -35,21 +42,45 @@ const Row = ({tittle, arr=[{
 
 const Home = () => {
 
-    const [upcomingMovies,setUpcomingMovies] = useState([])
+    const [upcomingMovies,setUpcomingMovies] = useState([]);
+    const [nowPlayingMovies,setNowPlayingMovies] = useState([]);
+    const [popularMovies,setPopularMovies] = useState([]);
+    const [topRatedMovies,setTopRatedMovies] = useState([]);
+    const [genre,setGenre] = useState([]);
 
     useEffect(() => {
   
   const fetchData = async()=>{
       const {data: {results} } =await axios.get(url+"/"+upcoming+"?api_key="+apikey);
       setUpcomingMovies(results);
-      // console.log(upcomingMovies);
-
+  };
+  const fetchNowPlaying = async()=>{
+      const {data: {results} } =await axios.get(url+"/"+nowplaying+"?api_key="+apikey);
+      setNowPlayingMovies(results);
+  };
+  const fetchPopular = async()=>{
+      const {data: {results} } =await axios.get(url+"/"+popular+"?api_key="+apikey);
+      setPopularMovies(results);
+  };
+  const fetchTopRated = async()=>{
+      const {data: {results} } =await axios.get(url+"/"+topRated+"?api_key="+apikey);
+      setTopRatedMovies(results);
+  };
+  const getAllGenre = async()=>{
+      const {data: {results} } =await axios.get(url+"/"+genre+"/movie/list"+"?api_key="+apikey);
+      setGenre(genre);
   };
    
-  fetchData()
+
+  getAllGenre();
+
+  fetchData();
+  fetchNowPlaying();
+  fetchPopular();
+  fetchTopRated();
 
   },[]);
-  
+
 
   return (
 <section className="Home">
@@ -58,12 +89,18 @@ const Home = () => {
 
 
  <Row tittle={"Upcoming Movies"} arr={upcomingMovies} />
- <Row tittle={"Popular On Netflix"} />
- <Row tittle={"Tv Shows"} />
- <Row tittle={"MOvies"} />
- <Row tittle={"Recent viewed"} />
- <Row tittle={"My List"} />
+ <Row tittle={"Now Playing"}     arr={nowPlayingMovies} />
+ <Row tittle={"Popular Movies"} arr={popularMovies} />
+ <Row tittle={"Top Rated Movies"} arr={topRatedMovies} />
+ 
 
+<div className="genreBox">
+  {genre.map((item)=>{
+
+    <Link key={item.id} to={"/"+genre+"/"+item.id} >${item.name}</Link>
+
+  })}
+</div>
 </section>
   )
 }
